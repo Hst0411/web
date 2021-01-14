fc8e59444c2794a8c8006f7c57144a6aa88e4df3
Hst0411 <73873452+Hst0411@users.noreply.github.com>
2021-01-14 20:08:51 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1121,6 +1121,10 @@ function restart(){
 }
 }
 //run file
+href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+//run file
 $(document).ready(function(){
     $("button,#xx").mouseover(function(){
         document.getElementById("buttonsound").play();
